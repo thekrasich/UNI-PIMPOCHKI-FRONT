717c2f773cf92023afbfa4cba222f07fc9e3f145
KkrisKu <kupyn.kristy@gmail.com>
2024-05-16 22:22:57 +0300

    bush

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import Poster from "../Poster/Poster";
 import Products from "../Products/Products";
+import Poster from './Poster/Poster';
 
 
 
@@ -10,7 +10,7 @@ const Home = () => {
     const { list } = useSelector((products) => products);
     return(
         <>
-            <Poster/>
+            <Poster />
             <Products products={list} amount={4} title="Trending" />
             
         </>
--- src/components/Home/Poster/Poster.jsx
+++ src/components/Home/Poster/Poster.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import styles from "./styles/Home.module.css";
+import styles from "./Home.module.css";
 
-const BG = "../images/casa.png";
+const BG = "../images/kasa.png";
 
 const Poster = () => (
   <section className={styles.home}>
