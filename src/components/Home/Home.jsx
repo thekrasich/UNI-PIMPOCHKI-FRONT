9402f8819af02a0e019942b343b2c53bc86de023
KkrisKu <kupyn.kristy@gmail.com>
2024-05-17 17:27:14 +0300

    init

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import Products from "../Products/Products";
-import Poster from './Poster/Poster';
-
+//import Poster from './Poster/Poster';
+//<Poster />
 
 
 const Home = () => {
     const { list } = useSelector((products) => products);
     return(
         <>
-            <Poster />
+            
             <Products products={list} amount={4} title="Trending" />
             
         </>
